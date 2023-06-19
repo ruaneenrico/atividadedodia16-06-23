a44d9e8c4667c8fc71bc6bfe56660761e873abf6
ruan <106768848+ruaneenrico@users.noreply.github.com>
2023-06-19 18:25:36 -0300

    Add files via upload

---
+++ Atividade do dia 16-06-2023/script.js
@@ -0,0 +1,41 @@
+function titulo(){
+    var titulo = window.document.getElementById('titulo')
+    var resp_ti = prompt('insira o titulo: ')
+    titulo.innerText = resp_ti
+}
+titulo()
+
+function subtitulo(){
+    var subtitulo = window.document.getElementById('subtitulo')
+    var resp_ti1 = prompt('insira o subtitulo: ')
+    subtitulo.innerText = resp_ti1
+}
+subtitulo()
+
+function itens1 (){
+    var itens1 = window.document.getElementById('itens1')
+    var resp_ti2 = prompt('insira o itens1: ')
+    itens1.innerText = resp_ti2
+}
+itens1()
+
+function itens2 (){
+    var itens2 = window.document.getElementById('itens2')
+    var resp_ti3 = prompt('insira o itens2: ')
+    itens2.innerText = resp_ti3
+}
+itens2()
+
+function itens3 (){
+    var itens3 = window.document.getElementById('itens3')
+    var resp_ti4 = prompt('insira o itens3: ')
+    itens3.innerText = resp_ti4
+}
+itens3()
+
+function itens4 (){
+    var itens4 = window.document.getElementById('itens4')
+    var resp_ti5 = prompt('insira o itens4: ')
+    itens4.innerText = resp_ti5
+}
+itens4()
